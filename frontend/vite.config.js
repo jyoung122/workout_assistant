3766b851344ff3f23c938a1b3c1203d4fb0ef511
jared-hawkins-young <143453407+jared-hawkins-young@users.noreply.github.com>
2025-03-04 23:37:25 -0500

    Fix Vite build process for GitHub Pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Determine the base URL based on the environment
-const BASE_URL =
-  import.meta.env.MODE === "production"
-    ? "/workout_assistant/" // For GitHub Pages
-    : "/"; // For local development
+// Check if we're in production based on Vite's mode
+// eslint-disable-next-line no-undef
+const isProduction = process.env.NODE_ENV === "production";
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ["**/*.gltf", "**/*.glb"],
-  base: BASE_URL
+  ase: isProduction ? "/workout_assistant/" : "/",
 })
