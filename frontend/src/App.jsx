06de505a78077e82444ff0dcc1cb84f4f65165c0
jared-hawkins-young <143453407+jared-hawkins-young@users.noreply.github.com>
2025-03-04 23:59:35 -0500

    Fix React Router basename

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/workout_assistant">
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Check if we're in production based on Vite's mode
-// eslint-disable-next-line no-undef
-const isProduction = process.env.NODE_ENV === "production";
-
+// Check if we're in production based on Vite's mod
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ["**/*.gltf", "**/*.glb"],
-  ase: isProduction ? "/workout_assistant/" : "/",
+  base:  "/workout_assistant/",
 })
